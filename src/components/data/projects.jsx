320d9f25141c48907b914a7fb27040fda4f938d9
Amir McCutchen <3amproduced@gmail.com>
2022-11-30 20:43:47 -0500

    Update projects.jsx

--- src/components/data/projects.jsx
+++ src/components/data/projects.jsx
@@ -9,15 +9,15 @@ export default [
         link: 'https://github.com/amirmccutchen/flashcards-app',
     },
     {
-        title: 'Encryption/Decryption App',
-        imgUrl: '../../images/decoderring.png',
-        stack: ['JavaScript', 'HTML', 'Test Driven Development'],
-        link: 'https://github.com/amirmccutchen/DECODER-RING',
-    },
-    {
         title: 'Developer Portfolio',
         imgUrl: '../../images/port.png',
         stack: ['JavaScript', 'React', 'CSS'],
         link: 'https://github.com/amirmccutchen/recipe-tracker',
+    },
+    {
+        title: 'Encryption/Decryption App',
+        imgUrl: '../../images/decoderring.png',
+        stack: ['JavaScript', 'HTML', 'Test Driven Development'],
+        link: 'https://github.com/amirmccutchen/DECODER-RING',
     },
 ];
