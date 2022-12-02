c883b298921d8b9fb9483c7bba93ccc1dc34415d
Amir McCutchen <3amproduced@gmail.com>
2022-12-02 10:24:04 -0500

    added my projects header

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -6,7 +6,7 @@ function Title({ children, id }) {
     return (
         <h1
           id = {id && id}
-          className = 'text-2xl font-bold text-zinc-900 dark:text-neutral-100 underline underline-offset-8 decoration-4 mb-10 text-center'>
+          className = 'text-2xl font-bold text-zinc-900 dark:text-neutral-100 underline underline-offset-8 decoration-4 mb-8 text-center'>
             {children}
           </h1>
     );
