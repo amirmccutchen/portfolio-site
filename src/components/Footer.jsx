eb7f6016220b4c08b74dd8e5babdf40f5adbaae3
Amir McCutchen <3amproduced@gmail.com>
2022-12-20 18:08:44 -0500

    added github social icon

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-
+import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
 // Replace my name on line 10 with your name
 
 function Footer() {
     return (
         <div className = 'py-3 text-center '>
             {/* add social icons here */}
+            <SocialIcon url="https://github.com/amirmccutchen" bgColor="#384a69" />
             <p className = 'text-sm mt-2 opacity-60'>
                 &copy; {new Date().getFullYear()} Amir McCutchen. <br /> Built with JavaScript, React, and TailwindCSS.
             </p>
