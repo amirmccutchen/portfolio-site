fea5d3f8a292ae21681fb61a326941ff76928f17
Amir McCutchen <3amproduced@gmail.com>
2022-12-22 00:06:44 -0500

    Update projects.jsx

--- src/components/data/projects.jsx
+++ src/components/data/projects.jsx
@@ -3,19 +3,19 @@ export default [
     {
         title: 'Study Tool Application',
         imgUrl: '../../images/flashcards.png',
-        stack: ['JavaScript', 'React', 'HTML', 'CRUD'],
+        stack: ['JavaScript', 'React', 'HTML', 'CRUD', 'Jest'],
         link: 'https://github.com/amirmccutchen/flashcards-app',
     },
     {
         title: 'Developer Portfolio',
         imgUrl: '../../images/port.png',
-        stack: ['JavaScript', 'React', 'CSS', 'Vite'],
+        stack: ['JavaScript', 'React', 'CSS', 'Vite', 'Jest'],
         link: 'https://github.com/amirmccutchen/portfolio-site',
     },
     {
         title: 'Movie Database Application',
         imgUrl: '../../images/movies.png',
-        stack: ['JavaScript', 'Express', 'Node.js', 'Knex', 'PostgreSQL'],
+        stack: ['JavaScript', 'Express', 'Node.js', 'Knex', 'PostgreSQL', 'Jest'],
         link: 'https://github.com/amirmccutchen/WeLoveMovies-Backend',
     },
 ];
