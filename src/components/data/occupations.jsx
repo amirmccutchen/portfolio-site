7c50b04e547f3c3756f3985069c3e857bc422b36
Amir McCutchen <3amproduced@gmail.com>
2023-01-09 10:27:08 -0500

    Update occupations.jsx

--- src/components/data/occupations.jsx
+++ src/components/data/occupations.jsx
@@ -2,21 +2,15 @@
 
 export default [
     {
-        year: '2022',
-        title: 'University of Delaware',
-        duration: '2 years',
-        details: `Once receiving my Associates from Delaware Tech, I moved on to the University of Delaware and graduated with a Bachelor's in Computer Science.`,
+        year: 'Today',
+        title: 'Software Engineering Student',
+        duration: '1 year',
+        details: `Desire and motivation to learn about what Software Development & Programming truly had to offer, I enrolled in Thinkful/Chegg's Software Engineering Immersion program. I studied and became proficient in the fundamentals of JavaScript, as well as HTML, CSS, PostgreSQL and frameworks such as React, Vite, TailwindCSS, ExpressJS, and Knex`,
     },
     {
-        year: '2020',
-        title: 'Delaware Technical Community College',
-        duration: '2 years',
-        details: `After building the business to a more hands-off state, I decided it was time to pursue a higher education in Computer Science.`,
-    },
-    {
-        year: '2016',
+        year: '2017',
         title: 'Composer & Entrepreneur',
-        duration: '6 years',
+        duration: '5 years',
         details: `Upon graduating high school, I developed a passion for music production, and taught myself Image Line's FL Studio Music Production software. Over the years as I grew my product and marketed myself through learning the algorithms of Instagram, I amassed over 44,000 followers, more than 3,000 unique clients, over 5 million streams, & licensed placements with entities such as Rick Ross, Logic, Trippie Redd,  Aubrey Joseph, Just Juice, Lil Tjay, and more.`,
     },
 ];
