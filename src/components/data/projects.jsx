e089588546485df64a6a12d7f0152240ac078367
Amir McCutchen <3amproduced@gmail.com>
2023-01-15 12:48:10 -0500

    added securefi to projects

--- src/components/data/projects.jsx
+++ src/components/data/projects.jsx
@@ -1,10 +1,16 @@
 // Add your portfolio projects here
 export default [
+    // {
+    //     title: 'Study Tool Application',
+    //     imgUrl: '../../images/flashcards.png',
+    //     stack: ['JavaScript', 'React', 'HTML', 'CRUD', 'Jest'],
+    //     link: 'https://github.com/amirmccutchen/flashcards-app',
+    // },
     {
-        title: 'Study Tool Application',
-        imgUrl: '../../images/flashcards.png',
-        stack: ['JavaScript', 'React', 'HTML', 'CRUD', 'Jest'],
-        link: 'https://github.com/amirmccutchen/flashcards-app',
+        title: 'SecureFi Landing Page',
+        imgUrl: '../../images/securefi.png',
+        stack: ['JavaScript', 'React', 'React Hooks', 'HTML', 'Tailwind', 'Vite'],
+        link: 'https://github.com/amirmccutchen/securefi-landing-page',
     },
     {
         title: 'Developer Portfolio',
