4c04ca066167bbdd09900fe45aba056f819462d3
Amir McCutchen <3amproduced@gmail.com>
2023-02-02 00:16:21 -0500

    Update occupations.jsx

--- src/components/data/occupations.jsx
+++ src/components/data/occupations.jsx
@@ -5,12 +5,20 @@ export default [
         year: 'Today',
         title: 'Software Engineering Student',
         duration: '1 year',
-        details: `Desire and motivation to learn about what Software Development & Programming truly had to offer, I enrolled in Thinkful/Chegg's Software Engineering Immersion program. I studied and became proficient in the fundamentals of JavaScript, as well as HTML, CSS, PostgreSQL and frameworks such as React, Vite, TailwindCSS, ExpressJS, and Knex`,
+        details: `Desire and motivation to learn about what Software Development & Programming truly had to offer, 
+        I enrolled in Thinkful/Chegg's Software Engineering Immersion program. 
+        I studied and became proficient in the fundamentals of JavaScript, as well as HTML, 
+        CSS, PostgreSQL and frameworks such as React, Vite, TailwindCSS, ExpressJS, and Knex`,
     },
     {
         year: '2017',
         title: 'Composer & Entrepreneur',
-        duration: '5 years',
-        details: `Upon graduating high school, I developed a passion for music production, and taught myself Image Line's FL Studio Music Production software. Over the years as I grew my product and marketed myself through learning the algorithms of Instagram, I amassed over 44,000 followers, more than 3,000 unique clients, over 5 million streams, & licensed placements with entities such as Rick Ross, Logic, Trippie Redd,  Aubrey Joseph, Just Juice, Lil Tjay, and more.`,
+        duration: '6 years',
+        details: `Upon graduating high school, I developed a passion for music production, 
+        and taught myself Image Line's FL Studio Music Production software. Over the years, 
+        as I grew my product and marketed myself through learning the algorithms of Instagram, 
+        I amassed over 44,000 followers, more than 3,000 unique clients, over 5 million streams,
+        & licensed placements with entities such as Rick Ross, Logic, Trippie Redd,  Aubrey Joseph, Just Juice, 
+        Lil Tjay, and more.`,
     },
 ];
