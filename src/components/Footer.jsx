94921f0044c5a5181e7d4a01305f8faa1113a707
Amir McCutchen <3amproduced@gmail.com>
2023-02-18 20:10:39 -0500

    font resizing for larger devices

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
             {/* add social icons here */}
             <SocialIcon url="https://github.com/amirmccutchen" bgColor="#525252" />
             <SocialIcon url="https://www.linkedin.com/in/amir-mccutchen/" bgColor="#525252" className="ml-4"/>
-            <p className = 'text-sm mt-5 font-bold dark:font-medium opacity-75 dark:opacity-60 font-sen'>
+            <p className = 'text-sm mt-5 font-medium opacity-80 dark:opacity-60 font-sen'>
                 &copy; {new Date().getFullYear()} Amir McCutchen. <br /> Built with JavaScript, React, and TailwindCSS.
             </p>
         </div>
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -10,9 +10,9 @@ function Introduction() {
             <img // insert your image here, resize if needed on line 12 using the w-x/12 starting with a mobile view, then work your way up
                src = '../images/me.jpg'
                alt = "Amir McCutchen" 
-               class = "rounded-full w-7/12 md:w-5/12 lg:w-3/12 mb-6"
+               class = "rounded-full w-5/12 md:w-4/12 lg:w-3/12 mb-6"
             />
-            <p className = 'text-md max-w-xl mb-6 font-semibold dark:font-medium font-sen dark:text-white'>
+            <p className = 'text-md max-w-xl mb-6 font-medium md:font-semibold dark:font-medium font-sen dark:text-white'>
                 I am a Full-Stack Software Developer working to provide modern solutions and create smooth, user-friendly applications. 
                 All projects are made to provide a streamlined, effective user experience in code written with an emphasis on cleanliness and legibility. 
                 Coming from an entreprenuerial background, I know that ease of use and a streamlined experience within an app is the key to converting a potential client into a frequent one.
