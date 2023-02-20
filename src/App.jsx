56b1e13c0dfd5b83b30cc38014898be5a8c415e9
Amir McCutchen <3amproduced@gmail.com>
2023-02-20 15:30:34 -0500

    updated contact form border color

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function App() {
       <button
         type = 'button'
         onClick = {handleThemeSwitch}
-        className="fixed z-10 right-1 md:right-10 top-4 bg-neutral-600 dark:bg-neutral-300 text-lg p-1 rounded-md"
+        className="fixed z-10 right-1 md:right-10 top-4 bg-darkblue dark:bg-neutral-300 text-lg p-1 rounded-md"
         >
           {theme === 'dark' ? sun : moon}
         </button>
