b57c70e4606c5d2d3460d16f188134d4eb7b4333
Amir McCutchen <3amproduced@gmail.com>
2023-02-27 16:05:30 -0500

    Update Occupations.jsx

--- src/components/Occupations.jsx
+++ src/components/Occupations.jsx
@@ -10,7 +10,7 @@ function Occupations() {
     return (
        <div className="flex flex-col md:flex-row justify-center my-8">
           <div className="w-full md:w-7/12">
-            <Title>Timeline</Title>
+            <Title>Experience</Title>
              {occupations.map(occupation => (
                 <OccupationItem 
                    year={occupation.year}
